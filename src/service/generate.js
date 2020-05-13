6ef31d217f899a3f598918c89bd45b96170ac99d
mignatyeva <margarita.ignatyeva@nctechimaging.com>
2020-05-13 23:21:10 +0100

    add readFile

--- src/service/generate.js
+++ src/service/generate.js
@@ -1,25 +1,35 @@
 'use strict';
 
 const fs = require(`fs`).promises;
+const path = require(`path`);
+const chalk = require(`chalk`);
 const {DateTime} = require(`luxon`);
 const utils = require(`../utils`);
 const errors = require(`../errorMessages`);
 const constants = require(`../constants`);
 
-const getUniqueIndexesArray = (limit, maxArrayLength) => {
-  const indexes = [];
-  while (indexes.length <= limit) {
-    const randomIndex = utils.getRandomArbitrary(0, maxArrayLength);
-    if (!indexes.includes(randomIndex)) {
-      indexes.push(randomIndex);
-    }
-  }
-  return indexes;
-};
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
-const generateArray = (limit, arr) => {
-  const randomIndexes = getUniqueIndexesArray(limit, arr.length);
-  return randomIndexes.map((i) => arr[i]);
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(path.resolve(`./src/service`, filePath), `utf-8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.log(chalk.red(err));
+    return [];
+  }
+}
+
+const generateOffers = (count, titles, categories, sentences) => {
+  return Array(parseInt(count, 10)).fill({}).map(() => ({
+    category: [categories[utils.getRandomArbitrary(1, categories.length - 1)]],
+    title: titles[utils.getRandomArbitrary(1, titles.length - 1)],
+    announce: utils.shuffle(sentences).slice(1, 5).join(` `),
+    fullText: utils.shuffle(sentences).slice(1, 5).join(` `),
+    createdDate: DateTime.local().minus({days: utils.getRandomArbitrary(0, 90)}).toFormat(`yyyy-MM-dd hh:mm:ss`),
+  }));
 };
 
 const generateMocks = async (count = 1) => {
@@ -27,23 +37,11 @@ const generateMocks = async (count = 1) => {
     return errors.generateMocksLimitExceeded();
   }
 
-  const offers = [];
-  for (let i = count; i > 0; i--) {
-    const title = utils.getRandomFromArray(constants.TITLES);
-    const announce = generateArray(utils.getRandomArbitrary(1, 5), constants.DESCRIPTIONS).join(` `);
-    const fullText = generateArray(utils.getRandomArbitrary(1, constants.DESCRIPTIONS.length), constants.DESCRIPTIONS).join(` `);
-    const createdDate = DateTime.local().minus({days: utils.getRandomArbitrary(0, 90)}).toFormat(`yyyy-MM-dd hh:mm:ss`);
-    const category = generateArray(utils.getRandomArbitrary(1, constants.CATEGORIES.length), constants.CATEGORIES);
+  const sentences = await readContent(FILE_SENTENCES_PATH);
+  const titles = await readContent(FILE_TITLES_PATH);
+  const categories = await readContent(FILE_CATEGORIES_PATH);
 
-    const offer = {
-      title,
-      announce,
-      fullText,
-      createdDate,
-      category
-    };
-    offers.push(offer);
-  }
+  const offers = generateOffers(count, titles, categories, sentences);
 
   try {
     await fs.writeFile(`mock.json`, JSON.stringify(offers, null, 4));
